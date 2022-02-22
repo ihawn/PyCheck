b1aecb59e0d73ff4011b93f94c36e08994bb0ce8
Isaac hawn <ihawn.pl15@gmail.comm>
2022-02-22 11:57:43 -0600

    movement and kinging

--- gamemanager.py
+++ gamemanager.py
@@ -4,6 +4,7 @@ import graphics
 import tkinter as tk
 from pygame.locals import *
 
+
 class GameManager:
     def __init__(self, width, height):
         self.game_running = True
@@ -11,19 +12,21 @@ class GameManager:
         self.height = height
         self.size = width, height
 
+
 def game_loop():
     root = tk.Tk()
     h = root.winfo_screenheight()
-    height = h*0.9
+    height = h * 0.9
 
     pygame.init()
     gm = GameManager(height, height)
     board = gameagents.Board()
     screen = pygame.display.set_mode(gm.size)
+    moves = []
+    piece = None
+    moved = False
 
-    surf = pygame.Surface((25, 25))
-    surf.fill((0, 200, 255))
-    rect = surf.get_rect()
+    graphics.draw_screen(screen, height, board, moves, piece)
     while gm.game_running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
@@ -32,4 +35,73 @@ def game_loop():
             elif event.type == QUIT:
                 gm.game_running = False
 
-        graphics.draw_screen(screen, height, board)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+
+                if piece != None:
+                    moves, board.piece_arr, moved, piece = move_piece(piece, board.piece_arr, moves, height, pos)
+                else:
+                    piece = piece_from_click(pos, board.piece_arr, height)
+
+                piece = piece_from_click(pos, board.piece_arr, height)
+                if not moved:
+                    moves = get_moves(piece, board.piece_arr, moves)
+
+                board.piece_arr = check_for_king(board.piece_arr)
+                graphics.draw_screen(screen, height, board, moves, piece)
+
+
+def coord_from_click(pos, size):
+    x = int(8 * pos[0] / size)
+    y = int(8 * pos[1] / size)
+    return x, y
+
+
+def piece_from_click(pos, piece_arr, size):
+    pos = coord_from_click(pos, size)
+    piece = piece_arr[pos[1]][pos[0]]
+    return piece
+
+
+def get_moves(piece, piece_arr, moves):
+    if piece.side != "empty":
+        moves = []
+        x = piece.x
+        y = piece.y
+        k = 1 if piece.side == "black" else -1
+
+        if 0 <= y - k < 8 and 0 <= x + k < 8 and piece_arr[y - k][x + k].side == "empty":
+            moves.append((x + k, y - k))
+        if 0 <= y - k < 8 and 0 <= x - k < 8 and piece_arr[y - k][x - k].side == "empty":
+            moves.append((x - k, y - k))
+        if piece.isking:
+            if 0 <= y + k < 8 and 0 <= x - k < 8 and piece_arr[y + k][x - k].side == "empty":
+                moves.append((x - k, y + k))
+            if 0 <= y + k < 8 and 0 <= x + k < 8 and piece_arr[y + k][x + k].side == "empty":
+                moves.append((x + k, y + k))
+    return moves
+
+
+def move_piece(piece, piece_arr, moves, size, clickpos):
+    moved = False
+    old_x = piece.x
+    old_y = piece.y
+    pos = coord_from_click(clickpos, size)
+    if pos in moves:
+        side = piece.side
+        is_king = piece.isking
+        piece_arr[piece.y][piece.x].side = "empty"
+        piece_arr[piece.y][piece.x].isking = False
+        piece_arr[pos[1]][pos[0]].side = side
+        piece_arr[pos[1]][pos[0]].isking = is_king
+        moves = []
+        moved = True
+    return moves, piece_arr, moved, piece
+
+def check_for_king(piece_arr):
+    for y in range(0, 8):
+        for x in range(0, 8):
+            p = piece_arr[y][x]
+            if (p.side == "white" and y == 7) or (p.side == "black" and y == 0):
+                piece_arr[p.y][p.x].isking = True
+    return piece_arr
--- graphics.py
+++ graphics.py
@@ -2,6 +2,7 @@ import pygame
 import gameagents
 from pygame import gfxdraw
 
+
 class Square(pygame.sprite.Sprite):
     def __init__(self, color, size):
         super(Square, self).__init__()
@@ -9,40 +10,72 @@ class Square(pygame.sprite.Sprite):
         self.surf.fill(color)
         self.rect = self.surf.get_rect()
 
-def draw_screen(screen, linearsize, board):
+
+def draw_screen(screen, linearsize, board, moves, piece):
+    screen.fill((0, 0, 0))
     draw_board(screen, linearsize)
+    if len(moves) > 0:
+        draw_potential_moves(screen, linearsize, moves, piece)
     draw_pieces(screen, board.piece_arr, linearsize)
     pygame.display.flip()
 
+
 def draw_board(screen, linearsize):
-    squarewidth = linearsize/8
-    blacksquare = Square((42, 109, 222), squarewidth*0.99)
-    whitesquare = Square((199, 220, 255), squarewidth*0.99)
+    squarewidth = linearsize / 8
+    blacksquare = Square((42, 109, 222), squarewidth * 0.99)
+    whitesquare = Square((199, 220, 255), squarewidth * 0.99)
 
     for x in range(0, 8):
         for y in range(0, 8):
             if gameagents.valid_square(x, y):
-                screen.blit(blacksquare.surf, (x*squarewidth, y*squarewidth))
+                screen.blit(blacksquare.surf, (x * squarewidth, y * squarewidth))
             else:
-                screen.blit(whitesquare.surf, (x*squarewidth, y*squarewidth))
+                screen.blit(whitesquare.surf, (x * squarewidth, y * squarewidth))
+
 
 def draw_pieces(screen, piece_arr, linearsize):
-    squarewidth = linearsize/8
+    squarewidth = linearsize / 8
     r = squarewidth * 0.35
     black = (10, 10, 10)
     white = (250, 250, 250)
+    crownsize = squarewidth*0.15
+
+    crown = Square((50, 50, 180), crownsize)
     border_b = (0, 0, 0)
     border_w = (255, 255, 255)
     for y in range(0, 8):
         for x in range(0, 8):
-            pos = (piece_arr[x][y].x*squarewidth + squarewidth/2, piece_arr[x][y].y*squarewidth + squarewidth/2)
+            pos = (piece_arr[x][y].x * squarewidth + squarewidth / 2, piece_arr[x][y].y * squarewidth + squarewidth / 2)
             if piece_arr[x][y].side == "black":
-                #pygame.draw.circle(screen, black, pos, r)
                 draw_circle(screen, pos, r, black)
             elif piece_arr[x][y].side == "white":
-                #pygame.draw.circle(screen, white, pos, r)
                 draw_circle(screen, pos, r, white)
+            if piece_arr[x][y].side != "empty" and piece_arr[x][y].isking:
+                crownpos = (pos[0] - crownsize/2, pos[1] - crownsize/2)
+                screen.blit(crown.surf, crownpos)
+
+
+def draw_potential_moves(screen, linearsize, moves, piece):
+    squarewidth = linearsize / 8
+    r = squarewidth * 0.38
+    highlight_color = (255, 60, 0)
+
+    if piece is not None:
+        pos = (piece.x*squarewidth + squarewidth/2, piece.y*squarewidth + squarewidth/2)
+        draw_circle(screen, pos, r, highlight_color)
+
+    for i in range(0, len(moves)):
+        x = moves[i][0]
+        y = moves[i][1]
+        draw_square_highlight(screen, linearsize, x, y)
+
 
 def draw_circle(surface, pos, r, color):
     gfxdraw.aacircle(surface, int(pos[0]), int(pos[1]), int(r), color)
     gfxdraw.filled_circle(surface, int(pos[0]), int(pos[1]), int(r), color)
+
+
+def draw_square_highlight(screen, linearsize, x, y):
+    squarewidth = linearsize / 8
+    highlightsquare = Square((0, 160, 255), squarewidth * 0.99)
+    screen.blit(highlightsquare.surf, (x * squarewidth, y * squarewidth))
